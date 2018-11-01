403beab069e4abbf540f021e5f0087fdfa3ef4f0
Kappeh <kieranleep@hotmail.com>
2018-11-01 22:34:32 +0000

    Bug fixes and basic command

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import sys
 import discord
 from config import OWNER
 from time import gmtime, strftime
+from src.commands import COMMANDS
 
 TOKEN = os.environ.get('DISCORD_TOKEN')
 if not TOKEN:
@@ -43,29 +44,30 @@ async def on_ready():
 
 @CLIENT.event
 async def on_message(message):
-    prefix = '<@' + CLIENT.user.id + '>'
+    prefix = '<@' + CLIENT.user.id + '> '
     user_command = ''
     if message.content.startswith(prefix):
         user_command = message.content.replace(prefix, '', 1)
     elif not message.server:
         user_command = message.content
-    
+
     if CLIENT.user.id != message.author.id and user_command:
-        log_message = str(message.author) + ' ran "' + user_command + '"'
+        log_message = str(message.author) + ' ran: "' + user_command + '"'
         if message.server:
             log_message += ' in server: ' + message.server.name
         else:
-            log_message += ' in private message'
+            log_message += ' in a private message'
         log_routine = log(log_message)
-        await log_routine
+        if LOG_USER['name'] != str(message.author) or message.server:
+            await log_routine
 
-    if user_command.lower().split(' ')[0] == 'help':
-        help_command = ' '.join(user_command.split(' ')[1:])
-        help_message = COMMANDS.get_help_message(help_command, CLIENT, help_command, message)
-        await CLIENT.send_message(message.channel, help_message)
-    else:
-        output = await COMMANDS.execute(user_command, CLIENT, user_command, message)
-        if isinstance(output, str):
-            await CLIENT.send_message(message.channel, output)
-            
+        if user_command.lower().split(' ')[0] == 'help':
+            argv = user_command.split(' ')[1:]
+            help_message = COMMANDS.get_help_message(*argv)
+            await CLIENT.send_message(message.channel, help_message)
+        else:
+            output = await COMMANDS.execute(user_command, CLIENT, user_command, message)
+            if isinstance(output, str):
+                await CLIENT.send_message(message.channel, output)
+
 CLIENT.run(TOKEN)
---
+++ src/commands.py
@@ -0,0 +1,9 @@
+from src.command_node import Command_Node
+from src.command_branch import Command_Branch
+
+COMMANDS = Command_Branch("KappehNet v1.0")
+
+async def ping(client, user_command, message):
+    await client.send_message(message.channel, ':ping_pong: Pong!! xSSS')
+
+COMMANDS.add_command('ping', Command_Node(ping, 'Replies with pong.'))
