c272dcd1635f095e6dc37d755105266ef58ae6e6
Kappeh <kieranleep@hotmail.com>
2018-11-01 19:11:29 +0000

    Added command class

---
+++ src/command.py
@@ -0,0 +1,18 @@
+class Command():
+    
+    _cmd_string = None
+    _brief = None
+    _description = None
+    _meta = {}
+
+    def __getitem__(self, i):
+        if i in self._meta:
+            return self._meta[i]
+        return None
+    
+    def get_help(self, args, description = False):
+        if full and self._description:
+            return self._description
+        elif self._brief:
+            return self._brief
+        return 'Error. Could not find command\'s help message.'
